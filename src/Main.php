32911d36fba2794d20b1f2c4d93535fb13d25497
QWER170 <149947869+QWER170@users.noreply.github.com>
2024-11-27 15:14:41 +0330

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,44 +1,67 @@
 <?php
 
-namespace GameParrot\PingView;
+namespace FarsiChat;
 
-use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat;
-use pocketmine\command\Command;
-use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 
-class Main extends PluginBase{
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		if($command->getName() === "ping"){
-				$target = $sender;
-				if (isset($args[0])) {
-					if ($sender instanceof Player && !$sender->hasPermission("pingview.others")) {
-						$sender->sendMessage(TextFormat::RED ."You do not have permission to view ping of others.");
-						return true;
-					}
-					if (Server::getInstance()->getPlayerExact($args[0])) {
-						$target = Server::getInstance()->getPlayerExact($args[0]);
-					} else {
-						$sender->sendMessage(TextFormat::RED."Could not find player ".$args[0]);
-						return true;
-					}
-				}
-				if ($target instanceof Player) {
-					$ping = $target->getNetworkSession()->getPing();
-					$color=TextFormat::GREEN;
-					if($ping > 170) {
-						$color= TextFormat::RED;
-					} else if ($ping > 85) {
-						$color= TextFormat::YELLOW;
-					}
-					$sender->sendMessage($target->getName()."'s ping: ".$color . $ping . TextFormat::WHITE ."ms");
-				} else {
-					$sender->sendMessage(TextFormat::RED."Not a player");
-				}
-			return true;
-		}
-		return false;
-	}
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("FarsiChat plugin has been enabled!");
+    }
+
+    public function onDisable(): void {
+        $this->getLogger()->info("FarsiChat plugin has been disabled!");
+    }
+
+    public function onChat(PlayerChatEvent $event): void {
+        $message = $event->getMessage();
+
+        // بررسی اینکه آیا متن حاوی حروف فارسی است
+        if ($this->containsFarsi($message)) {
+            // اصلاح و معکوس کردن متن فارسی
+            $fixedMessage = $this->fixFarsiText($message);
+            $event->setMessage($fixedMessage);
+        }
+    }
+
+    private function containsFarsi(string $text): bool {
+        // بررسی وجود کاراکترهای فارسی در متن
+        return preg_match('/[آ-ی]/u', $text) > 0;
+    }
+
+    private function fixFarsiText(string $text): string {
+        // معکوس کردن متن
+        $reversed = implode('', array_reverse(mb_str_split($text)));
+
+        // اصلاح چسبندگی حروف فارسی (در صورت نیاز)
+        return $this->joinFarsiLetters($reversed);
+    }
+
+    private function joinFarsiLetters(string $text): string {
+        // جدول ساده چسبندگی حروف فارسی
+        $lettersMap = [
+            'ا' => 'ﺍ', 'ب' => 'ﺑ', 'پ' => 'ﭘ', 'ت' => 'ﺗ',
+            'ث' => 'ﺛ', 'ج' => 'ﺟ', 'چ' => 'ﭼ', 'ح' => 'ﺣ',
+            'خ' => 'ﺧ', 'د' => 'ﺩ', 'ذ' => 'ﺫ', 'ر' => 'ﺭ',
+            'ز' => 'ﺯ', 'ژ' => 'ﮊ', 'س' => 'ﺳ', 'ش' => 'ﺷ',
+            'ص' => 'ﺻ', 'ض' => 'ﺿ', 'ط' => 'ﻃ', 'ظ' => 'ﻇ',
+            'ع' => 'ﻋ', 'غ' => 'ﻏ', 'ف' => 'ﻓ', 'ق' => 'ﻗ',
+            'ک' => 'ﻛ', 'گ' => 'ﮔ', 'ل' => 'ﻟ', 'م' => 'ﻣ',
+            'ن' => 'ﻧ', 'و' => 'ﻭ', 'ه' => 'ﻫ', 'ی' => 'ﯾ',
+        ];
+
+        $output = '';
+        $length = mb_strlen($text);
+
+        for ($i = 0; $i < $length; $i++) {
+            $char = mb_substr($text, $i, 1);
+            $output .= $lettersMap[$char] ?? $char; // جایگزینی حرف با نسخه چسبیده آن
+        }
+
+        return $output;
+    }
 }
